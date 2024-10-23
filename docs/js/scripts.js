e80e87a52c06d681aa50d506cfbe8de9dc0542cc
kovalj1337 <vanaplayers@gmail.com>
2024-10-23 15:47:51 +0300

    ddd

---
+++ docs/js/scripts.js
@@ -0,0 +1,1 @@
+const bord=document.querySelector(".bord"),cancel=document.querySelector(".cancel-burger"),bgcBlack=document.querySelector(".bgc-black"),nav=document.querySelector(".navigation.light nav");bord.addEventListener("click",(function(){nav.style.left="0",bgcBlack.style.right="-60%"})),cancel.addEventListener("click",(function(){nav.style.left="-100%",bgcBlack.style.right="-100%"}));
